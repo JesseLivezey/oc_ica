62f53b1312f0b4862fe1e312a598ebb6386e8ba6
afbujan <afbujan@berkeley.edu>
2016-06-15 20:29:06 -0700

    working on the plotting functions

--- compute_angle_dist.py
+++ compute_angle_dist.py
@@ -1,26 +1,16 @@
-import theano
-import theano.tensor as T
-from theano.compat.python2x import OrderedDict
-from numpy.linalg import inv,norm
-from scipy.optimize import minimize
-import matplotlib
-
 import numpy as np
-import matplotlib.pyplot as plt
-
+from numpy.linalg import norm
 import ica_classes as ica_c
 reload(ica_c)
-
 
 overcompleteness = 2
 n_mixtures = 64
 n_sources = n_mixtures*overcompleteness
-initial_conditions = ['random','pathological']#['random','collimated','pathological']
+initial_conditions = ['random','collimated','pathological']
 degeneracy_controls = ['L2','COULOMB','RANDOM','L4','COULOMB_A']
 
 def decorr(X):
     return (3./2.)*X - (1./2.)*X.dot(X.T).dot(X)
-
 
 def get_Winit(n_sources, n_mixtures, init=random):
     if init=='random':
@@ -31,9 +21,8 @@ def get_Winit(n_sources, n_mixtures, init=random):
     elif init=='pathological':
         w = np.tile(np.eye(n_mixtures), (n_sources//n_mixtures, 1))+\
             np.random.randn(n_sources, n_mixtures)/100.
-    w = w/np.linalg.norm(w_0, axis=-1, keepdims=True)
+    w = w/norm(w_0, axis=-1, keepdims=True)
     return w
-
 
 def get_W(initial_conditions, degeneracy_controls, n_sources, n_mixtures):
     """Obtain W that minimizes the ICA loss funtion without the penalty"""
@@ -49,6 +38,12 @@ def get_W(initial_conditions, degeneracy_controls, n_sources, n_mixtures):
                 W[i,j] = ica.fit(X).components_
             else:
                 wd = decorr(W_0[i].copy())
-                wd = wd/np.linalg.norm(wd, axis=-1, keepdims=True)
+                wd = wd/norm(wd, axis=-1, keepdims=True)
                 W[i,j] = wd
     return W, W_0
+
+def compute_angles(w):
+    w = w/norm(w, axis=-1, keepdims=True)
+    gram = w.dot(w.T)
+    gram_off_diag = gram[np.tri(gram.shape[0], k=-1, dtype=bool)]
+    return np.arccos(abs(gram_off_diag))/np.pi*180.
